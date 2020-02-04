fb1bfec7681ab614fd328d08810aa62bb78f736d
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-02-04 22:33:36 +0530

    Changed the UI for showing the todo items
    * When the uset will click on the title
      it will show the items

--- public/scripts/loadHome.js
+++ public/scripts/loadHome.js
@@ -9,23 +9,21 @@ const sendHttpGetReq = function(url, callback){
   req.send();
 };
 
-const structureTodo = function(todoString){};
-
 const structureItems = function(items){
   return items.reduce((html, item) => { 
-    return `${html}</br><input type="checkbox">${item.item}`; 
+    return `${html}</br><input type="checkbox" id="${item.id}">${item.item}`; 
   }, '');
 };
 
 const formateTodo = function(todoHtml, todo){
   const time = new Date(todo.time).toLocaleString();
   return `${todoHtml}
-        <div class="extractedTodo flex" id="${todo.id}" >
+        <div class="extractedTodo flex" id="${todo.id}" onclick="showItems()" >
           <span>
             <h3>${todo.title}</h3>
           </span>${time}
         </div>
-        <div>
+        <div class="items">
           ${structureItems(todo.items)}
         </div></br>`;
 };
@@ -35,6 +33,15 @@ const structureTodoList = function(todoListString) {
   const html = todoList.reduce(formateTodo, '');
   const parent = document.getElementById('todoList');
   parent.innerHTML = html;
+};
+
+const showItems = function () {
+  const content = event.target.nextElementSibling;
+  if (content.style.display === 'block') {
+    content.style.display = 'none';
+  } else {
+    content.style.display = 'block';
+  }
 };
 
 const main = function(){
