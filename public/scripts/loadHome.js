4f3880bb2079c727fa9bd12e30ab3931d2222f01
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-02-04 20:44:14 +0530

    Showing todo items in the home page with the title

--- public/scripts/loadHome.js
+++ public/scripts/loadHome.js
@@ -9,12 +9,24 @@ const sendHttpGetReq = function(url, callback){
   req.send();
 };
 
+const structureTodo = function(todoString){};
+
+const structureItems = function(items){
+  return items.reduce((html, item) => { 
+    return `${html}</br><input type="checkbox">${item.item}`; 
+  }, '');
+};
+
 const formateTodo = function(todoHtml, todo){
   const time = new Date(todo.time).toLocaleString();
-  return `${todoHtml}<div class="extractedTodo flex">
+  return `${todoHtml}
+        <div class="extractedTodo flex" id="${todo.id}" >
           <span>
             <h3>${todo.title}</h3>
           </span>${time}
+        </div>
+        <div>
+          ${structureItems(todo.items)}
         </div></br>`;
 };
 
