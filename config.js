9dbecd9ec462d54acf747d651a5029a66b3cc5e3
ayush <58028488+desibabua@users.noreply.github.com>
2020-02-12 18:59:09 +0530

    [ayush | sruthy] added dataStore path in npm start of package.json

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-const getDataStorePath = function(){
-  return process.env.STORAGE_FILE || `${__dirname}/dataStore/todoInfo.json`;
-};
-
 const config = {
-  STORAGE_FILE: getDataStorePath()
+  STORAGE_FILE: process.env.STORAGE_FILE
 };
 
 module.exports = config;
