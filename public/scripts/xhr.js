b886335a63203b9b40cee49c57c75fceae2a7868
ayush <58028488+desibabua@users.noreply.github.com>
2020-02-14 12:53:13 +0530

    [sruthy | ayush] added hasfield handler
    for checking required fields in every request

--- public/scripts/xhr.js
+++ public/scripts/xhr.js
@@ -1,36 +1,31 @@
-const sendReq = function(method, url, callback) {
+const sendReq = function(method, url, body, callback) {
   const req = new XMLHttpRequest();
   req.onload = function() {
-    if (this.status === 200) {
+    if (this.status < 400) {
       return callback(this.responseText);
     }
   };
   req.open(method, url);
-  req.send();
-};
-
-const sendReqAsJson = function(method, url, body, callback) {
-  const req = new XMLHttpRequest();
-  req.onload = function() {
-    return callback(this.responseText);
-  };
-  req.open(method, url);
-  req.setRequestHeader('Content-Type', 'application/json');
-  req.send(JSON.stringify(body));
+  let content = body;
+  if (body) {
+    req.setRequestHeader('Content-Type', 'application/json');
+    content = JSON.stringify(body);
+  }
+  req.send(content);
 };
 
 const sendHttpGetReq = function(url, callback) {
-  sendReq('GET', url, callback);
+  sendReq('GET', url, '', callback);
 };
 
 const sendHttpPostReq = function(url, body, callback) {
-  sendReqAsJson('POST', url, body, callback);
+  sendReq('POST', url, body, callback);
 };
 
 const sendHttpPatchReq = function(url, body, callback) {
-  sendReqAsJson('PATCH', url, body, callback);
+  sendReq('PATCH', url, body, callback);
 };
 
 const sendHttpDeleteReq = function(url, body, callback) {
-  sendReqAsJson('DELETE', url, body, callback);
+  sendReq('DELETE', url, body, callback);
 };
