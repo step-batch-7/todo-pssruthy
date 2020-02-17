3dc816ced5f3b4d2cead89a8ec9b9d47666d7529
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-02-17 18:45:29 +0530

    [Aysuh | Sruthy] moved all global variables to app.locals

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+const { readFileSync, writeFileSync, existsSync, statSync } = require('fs');
 const { app } = require('./lib/appRouters');
+const { Users } = require('./lib/users');
+const { UserTodo } = require('./lib/userTodo');
+const config = require('./config');
+const { storeData, loadData } = require('./lib/fileOperations');
+
+const readData = loadData.bind(null, existsSync, statSync, readFileSync);
+const storeTodoList = storeData.bind(null, writeFileSync, config.STORAGE_FILE);
+const storeUsers = storeData.bind(null, writeFileSync, config.USERS_DETAIL);
+
+app.locals = {
+  users: Users.load(readData(config.USERS_DETAIL)),
+  userTodo: UserTodo.load(readData(config.STORAGE_FILE)),
+  sessionIds: {},
+  storeTodoList,
+  storeUsers
+};
 
 const port = 8080;
 app.listen(port, () => process.stdout.write(`Started listening on ${port}`));
